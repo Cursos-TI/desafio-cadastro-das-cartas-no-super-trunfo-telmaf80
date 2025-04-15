2c8d73f66815a60953fcdcb3d9895b593eb02706
Maria Telma Barbosa De Freitas <telmaf80@gmail.com>
2025-04-15 01:20:51 +0000

    primeiros comandos

--- desafiocadastrocartas.c/desafiocartas.c
+++ desafiocadastrocartas.c/desafiocartas.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 int main(){
     printf("Desafio Super Trunfo!\n");
+    char estado[20];
+    char codigo[20];
+    char cidade[20];
+    int habitantes;
+    float area;
+    float pib;
+    int pontosturisticos;
+
+    printf("Digite o código da carta: ");
+    scanf("%s", &codigo);
+    print("O codigo da carta é: %s\n");
 
 
 
